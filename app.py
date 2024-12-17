4f7f5184d8a2aaf9179eb1dcfe51bc595963ddc6
SangwanSiddharth <ch.siddharthsangwan8@gmail.com>
2024-12-17 12:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
+from sklearn.preprocessing import PowerTransformer as pt
 
 with open ('final_model_rf.pkl','rb') as file:
     model= pickle.load(file)
@@ -11,7 +12,6 @@ with open('transformer.pkl','rb') as file:
     pt= pickle.load(file)
 
 def prediction(input_list):
-    input_list=np.array(input_list,dtype=object)
 
     pred = model.predict_proba([input_list])[:,1][0]
 
@@ -36,7 +36,7 @@ def main():
     wkday_lambda=(lambda x:9 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thus' else 4 if x=='Fri' else 5 if x=='Fri' else 6 if x=='Sat' else 6)
     wkday=st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun'])
 
-    trans_data=pt.transform([lt,price])
+    trans_data=pt.transform([[lt,price]])
     lt_t=trans_data[0][0]
     price_t=trans_data[0][1]
 
