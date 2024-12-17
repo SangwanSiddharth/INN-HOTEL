cada53ef14e2cd56c62f0985a1cb9b46cbd8f26d
SangwanSiddharth <ch.siddharthsangwan8@gmail.com>
2024-12-17 13:14:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.preprocessing import PowerTransformer as pt
 
 # Load the model and transformer
 with open('final_model_rf.pkl', 'rb') as file:
@@ -13,6 +12,7 @@ with open('transformer.pkl', 'rb') as file:
 
 # Define the prediction function
 def prediction(input_list):
+    input_list = np.array(input_list,dtype='object')
     pred = model.predict_proba([input_list])[:,1][0]
     if pred > 0.5:
         return f'This booking is more likely to get cancelled: chances {round(pred, 2)}'
@@ -23,18 +23,18 @@ def prediction(input_list):
 def main():
     st.title('INN HOTEL GROUPS')
 
-    lt_input = st.number_input('Enter the lead time.',format='%.4f')
+    lt_input = st.text_input('Enter the lead time.')
 
     mst = (lambda x: 1 if x == 'Online' else 0)(st.selectbox('Choose the type of booking', ['Online', 'Offline']))
     spcl = st.selectbox('Select the no. of special requests made', [0, 1, 2, 3, 4, 5])
     
-    price_input = st.number_input('Enter the price offered for the room.',format='%.4f')
+    price_input = st.text_input('Enter the price offered for the room.')
     
     adult = st.radio('Select the number of adults in booking', [0, 1, 2, 3, 4])
 
-    wkd_input = st.number_input('Enter the number of weekend nights in the booking',format='%.4f')
+    wkd_input = st.text_input('Enter the number of weekend nights in the booking')
 
-    wk_input = st.number_input('Enter the number of week nights in the booking',format='%.4f')
+    wk_input = st.text_input('Enter the number of week nights in the booking')
 
     park = (lambda x: 1 if x == 'Yes' else 0)(st.selectbox('Is parking included in the booking', ['Yes', 'No']))
     month = st.slider('What will be the month of arrival', min_value=1, max_value=12, step=1)
