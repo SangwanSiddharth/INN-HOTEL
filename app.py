8abc1036707b4059a07a69c7bc7aa867b870a7d3
SangwanSiddharth <ch.siddharthsangwan8@gmail.com>
2024-12-23 15:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,18 +31,18 @@ def main():
     mst = (lambda x:1 if x=='Online' else 0)(st.selectbox('Enter the type of booking',['Online','Offline']))
     spcl = st.selectbox('Select the no of special requests made',[0,1,2,3,4,5])
     price = st.text_input('Enter the price offered for the room')
-    adult = st.selectbox('Select the no adults in booking',[0,1,2,3,4])
-    wkd = st.text_input('Enter the weekend nights in the booking')
-    wk = st.text_input('Enter the week nights in booking')
-    park = (lambda x:1 if x=='Yes'else 0)(st.selectbox('Is parking included in the booking',['Yes','No']))
+    adults = st.selectbox('Select the no adults in booking',[0,1,2,3,4])
+    weekend = st.text_input('Enter the weekend nights in the booking')
+    weekday = st.text_input('Enter the week nights in booking')
+    parking = (lambda x:1 if x=='Yes'else 0)(st.selectbox('Is parking included in the booking',['Yes','No']))
     month = st.slider('What will be month of arrival',min_value=1,max_value=12,step=1)
     day = st.slider('What will be day of arrival',min_value=1,max_value=31,step=1)
     wkday_lambda = (lambda x: 0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thus' 
                     else 4 if x=='Fri' else 5 if x=='Sat' else 6)
-    wkday = wkday_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
+    weekd = wkday_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
 
     
-    inp_list = [lt,mst,spcl,price,adult,wkd,park,wk,month,day,wkday]
+    inp_list = [lt,mst,spcl,price,adults,weekend,parking,weekday,month,day,weekd]
     
     if st.button('Predict'):
         response = prediction(inp_list)
